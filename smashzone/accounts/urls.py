ad3fb3dd4ecf00d7a53970146ca61def3fd2dd01
Lonewolfrex <samparkprakash27@gmail.com>
2024-07-09 13:16:54 +0530

    Registration and login feature updated

--- smashzone/accounts/urls.py
+++ smashzone/accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import register_view
-from .views import UserList, UserDetail
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', register_view, name='register'),
-    path('users/', UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-]
+    # path('users/', UserList.as_view(), name='user-list'),
+    # path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='registration/login.html',
+        redirect_authenticated_user=True  # Redirects already logged-in users
+    ), name='login'),
+    ]
